8aceb520a84de48bc83453a09c112cecaf02f25a
kayspark <kspark@nepes.co.kr>
2024-03-02 22:09:41 +0900

    daily commit

---
+++ lua/plugins/orgmode.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    "nvim-orgmode/orgmode",
+    dependencies = {
+      {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+      },
+    },
+    event = "BufReadPre",
+    config = function()
+      require("orgmode").setup_ts_grammar()
+      require("nvim-treesitter.configs").setup({
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = { "org" },
+        },
+        ensure_installed = "org",
+      })
+
+      require("orgmode").setup({
+        org_todo_keywords = { "🟡(t)", "🟢(p)", "🔴(h)", "|", "☑️(d)", "🟠(w)" },
+        org_todo_keyword_faces = {
+          --        DELEGATED = ':background #FFFFFF :slant italic :underline on',
+          TODO = ":background #000000 :foreground #E78284", -- overrides builtin color for `TODO` keyword
+        },
+        mappings = {
+          text_objects = {
+            inner_heading = "ic",
+            around_heading = "ac",
+            inner_subtree = "is",
+            around_subtree = "as",
+            inner_heading_from_root = "iC",
+            around_heading_from_root = "aC",
+            inner_subtree_from_root = "iS",
+            around_subtree_from_root = "aS",
+          },
+        },
+        emacs_config = {
+          excutable_path = "~/.emacs.d/bin/emacs",
+          config_path = "~/.congig/doom/config.el",
+        },
+        org_agenda_files = { "~/org/*.org", "~/org/org-roam/**/*" },
+        org_default_notes_file = "~/org/notes.org",
+        org_capture_templates = {
+          T = {
+            description = "Todo",
+            template = "* 🟡 %?\n %u",
+            target = "~/Org/todo.org",
+          },
+          n = {
+            description = "Notes",
+            template = "\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?",
+            target = "~/Org/notes.org",
+          },
+        },
+      })
+    end,
+  },
+}
