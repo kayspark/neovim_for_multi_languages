cf60aa2f940a7c27aa2be9dea0c7c651dbe186c7
kayspark <kspark@nepes.co.kr>
2024-03-22 20:43:39 +0900

    daily fixes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- set deault input method to koreans
+vim.g.input_method_default = "korean"
 local opt = vim.opt
 
 opt.foldlevel = 20
@@ -32,7 +34,7 @@ opt.splitkeep = "cursor"
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
-
+--
 -- Add asterisks in block comments
 opt.formatoptions:append({ "r" })
 
