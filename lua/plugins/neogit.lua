1a1bd7d6f9fee330a34f254121c92c005eaad9d0
kayspark <kspark@nepes.co.kr>
2026-02-17 19:09:21 +0900

    Simplify Git UX and disable claudecode extra

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,22 +1,31 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "sindrets/diffview.nvim",
-  },
-  cmd = "Neogit",
-  opts = {
-    signs = {
-      hunk = { "", "" },
-      item = { "", "" },
-      section = { "", "" },
-    },
-    integrations = {
-      diffview = true,
+  {
+    "folke/snacks.nvim",
+    keys = {
+      { "<leader>gg", false },
+      { "<leader>gG", false },
     },
   },
-  keys = {
-    { "<leader>gc", "<cmd>lua require('neogit').open({'commit'})<CR>", desc = "Git commit" },
-    { "<leader>gg", "<cmd>lua require('neogit').open()<CR>", desc = "Git status" },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+    },
+    cmd = "Neogit",
+    opts = {
+      signs = {
+        hunk = { "", "" },
+        item = { "", "" },
+        section = { "", "" },
+      },
+      integrations = {
+        diffview = true,
+      },
+    },
+    keys = {
+      { "<leader>gc", "<cmd>lua require('neogit').open({'commit'})<CR>", desc = "Neogit commit" },
+      { "<leader>gg", "<cmd>lua require('neogit').open()<CR>", desc = "Neogit status" },
+    },
   },
 }
